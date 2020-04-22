4f10addfbdd095cca7f8826ed3ac5971ed0c1171
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-22 21:11:33 +0545

    Input Validation

---
+++ ArrayPractice.js
@@ -0,0 +1,10 @@
+// Concat
+function ArrayPractice() {
+	let Laptops = ["HP", "Dell", "MAC"];
+	let Mobile = ["SAMSUNG", "IPHONE", "ONE PLUS"]
+	let Gatgets = Laptops.concat(Mobile);
+	console.log(Gatgets);
+}
+ArrayPractice();
+
+// Result: "HP", "Dell", "MAC", "SAMSUNG", "IPHONE", "ONE PLUS"
